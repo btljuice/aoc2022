adc35afc4df1c18e4872cf7e62d9f865be0ff180
Alexis Trudeau <atrudeau@hopper.com>
2022-12-17 11:09:31 -0500

    Day15 part 2 - wip

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,10 +1,17 @@
 module Day15 (
-  day15part1
+  day15part1,
+  day15part2,
+  merge,
+  readSensor,
+  holesInRanges,
+  holesInBound,
+  insert
 ) where
 
-import Lib(submatches)
+import Lib(submatches, sliding)
 import Data.Set(difference)
-import Data.Maybe(maybe)
+import Data.Maybe(maybe, fromJust, isJust)
+import Control.Exception (assert)
 import qualified Data.Set as Set
 
 
@@ -24,12 +31,23 @@ type Range = (Int, Int)
 
 data Sensor = Sensor { sensorPos :: Coord, beaconPos :: Coord, manDist :: Int } deriving (Show)
 
-merge :: Range -> Range -> [Range]
+merge :: Range -> Range -> (Range, Maybe Range)
 merge a@(a0, a1) b@(b0, b1)
   | a0 > b0 = merge b a
-  | a1 < b0 = [a, b]
-  | a1 < b1 = [(a0, b1)]
-  | otherwise = [a]
+  | a `lt` b = (a, Just b)
+  | a1 < b1 = ((a0, b1), Nothing)
+  | otherwise = (a, Nothing)
+
+lt :: Range -> Range -> Bool
+lt (_, a1) (b0, _) = a1 < b0 - 1
+
+insert :: Range -> [Range] -> [Range]
+insert r [] = [r]
+insert r0 (r1:t)
+  | r0 `lt` r1 = r0 : r1 : t
+  | otherwise = case merge r0 r1 of
+    (r', Just r'') -> r' : insert r'' t
+    (r', Nothing) -> insert r' t
 
 
 coordRegex = "x=(-?[[:digit:]]+), y=(-?[[:digit:]]+)"
@@ -58,6 +76,7 @@ noBeaconRange Sensor { sensorPos = (sx, sy), manDist = md } y
   where dy = abs (sy - y)
         nbPoints = md - dy
 
+-- OPT ME: Use Range instead of points
 nbNoBeaconPos :: [Sensor] -> Int -> Int
 nbNoBeaconPos sensors y = Set.size ( noXs `difference` beaconXs )
   where noXs = Set.fromList . concatMap (`noBeaconXs` y) $ sensors
@@ -67,8 +86,52 @@ day15part1 :: [String] -> Int -> Int
 day15part1 sensors y = (`nbNoBeaconPos` y) . fmap readSensor $ sensors
 
 
--- Part 2
+------ Part 2
 -- (x, y) E [0, 4M]
 
-tuningFreq :: Sensor -> Int
-tuningFreq Sensor{beaconPos = (x, y)} = x*4000000 + y --
+noBeaconRanges :: [Sensor] -> Int -> [Range]
+noBeaconRanges sensors y = foldl noRanges [] sensors
+  where noRanges rgs s = concatRanges rgs (noBeaconRange s y)
+        concatRanges rgs (Just r) = insert r rgs
+        concatRanges rgs Nothing  =  rgs
+
+holesInRanges :: [Range] -> [Int]
+holesInRanges = concatMap holes . sliding 2
+  where holes ((x0, x1):(y0, _):_) = assert (x1 < y0 - 1) [x1 + 1 .. y0 - 1]
+
+boundRanges :: Int -> Int -> [Range] -> [Range]
+boundRanges from to = insert minRange . insert maxRange
+  where minRange = (minBound :: Int, from - 1)
+        maxRange = (to + 1, maxBound :: Int)
+
+holesInBound :: Int -> Int -> [Sensor] -> Int -> [Int]
+holesInBound from to sensors y =  holesInRanges (boundRanges from to (noBeaconRanges sensors y))
+
+day15part2 :: [String] -> Int -> Int -> (Int, Coord)
+day15part2 sensorStrs from to = (tuned . head) [(fromJust x, y) | y <- [from..to], let x = hole y, isJust x ]
+  where sensors = fmap readSensor sensorStrs
+        hole = firstHole . holesInBound from to sensors
+        firstHole [] = Nothing
+        firstHole [h] = Just h
+        firstHole _ = error "Only expecting one hole"
+        tuned c = (tuningFreq c, c)
+
+
+tuningFreq :: Coord -> Int
+tuningFreq (x, y) = x*4000000 + y
+
+sensorStrs1 = [
+  "Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+  "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+  "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+  "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+  "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+  "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+  "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+  "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+  "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+  "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+  "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+  "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+  "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+  "Sensor at x=20, y=1: closest beacon is at x=15, y=3" ]
--- test/Day15Spec.hs
+++ test/Day15Spec.hs
@@ -19,8 +19,29 @@ sensorStrs = [
   "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
   "Sensor at x=20, y=1: closest beacon is at x=15, y=3" ]
 
+sensors = fmap readSensor sensorStrs
+
 spec :: Spec
 spec = do
   describe "day15part1" $ do
     it "should solution correctly" $ do
       day15part1 sensorStrs 10 `shouldBe` 26
+  describe "merge" $ do
+    it "should merge correctly following ranges" $ do
+      merge (0, 1) (3, 4) `shouldBe` ((0, 1), Just (3, 4))  -- Don't touch
+      merge (0, 1) (2, 4) `shouldBe` ((0, 4), Nothing)  -- touch
+      merge (-3, 1) (0, 4) `shouldBe` ((-3, 4), Nothing) -- overlaps
+      merge (-3, 5) (0, 4) `shouldBe` ((-3, 5), Nothing) -- contains
+      --- Inverted position
+      merge (3, 4) (0, 1)  `shouldBe` ((0, 1), Just (3, 4))  -- Don't touch
+      merge (2, 4) (0, 1)  `shouldBe` ((0, 4), Nothing)  -- touch
+      merge (0, 4) (-3, 1) `shouldBe` ((-3, 4), Nothing) -- overlaps
+      merge (0, 4) (-3, 5) `shouldBe` ((-3, 5), Nothing) -- contains
+
+  describe "holesInBound" $ do
+    it "should find the right holes in the sample" $ do
+      holesInBound 0 20 sensors 11 `shouldBe` [14]
+
+  describe "day15part2" $ do
+    it "should solution correctly" $ do
+      day15part2 sensorStrs 0 20 `shouldBe` (56000011, (14, 11))
