c5c061d7ca29fb26e6e79041cb7dbf54379fbe70
Alexis Trudeau <atrudeau@hopper.com>
2022-12-20 07:51:35 -0500

    Day 16 WIP

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,12 +1,15 @@
 module Day16 (
   readValve,
   Valve(..),
+  Label(..),
   mkValve,
+  mkLabel,
+  shortestPaths
 ) where
 
 import Lib(submatches3, shortestPath)
 import Data.List.Split(splitOn)
-import Data.Array(Array)
+import Data.Array(Array, Ix)
 import qualified Text.Read
 
 -- 30 minutes before volcano erupts
@@ -18,10 +21,12 @@ type Cost = Int
 
 -- TODO convert to data Label = Label Int
 -- make Read and Show for 2 chars
-data Label = Label Int deriving (Eq)-- 2 Char has 26*26 = 676 possibilities
+data Label = Label Int deriving (Eq, Ord, Ix)-- 2 Char has 26*26 = 676 possibilities
 
 instance Show Label where
-  show (Label i) = show i
+  show (Label i) = [a, b]
+    where a = toEnum (i `div` 26 + fromEnum 'A') :: Char
+          b = toEnum (i `mod` 26 + fromEnum 'A') :: Char
 
 instance Read Label where
   readPrec = do
@@ -29,6 +34,11 @@ instance Read Label where
     b <- Text.Read.get
     let toInt c = fromEnum c - fromEnum 'A'
     return (Label (26 * toInt a + toInt b))
+
+mkLabel :: String ->  Label
+mkLabel = read
+
+startLabel = mkLabel "AA"
 
 
 type FlowRate = Int
@@ -53,9 +63,9 @@ readValve str = Valve (read valveLabel) (read flowRate) labels
         labels = fmap read . splitOn ", " $ lbls
 
 shortestPaths :: [Valve] -> [[Label]]
-shortestPaths valves = shortestPath edges <$> non0Labels <*> non0Labels
+shortestPaths valves = filter (\ p -> length p > 1) ( shortestPath edges <$> non0Labels <*> non0Labels )
   where edges = concat [[(from, to), (to,  from)] | Valve from _ tos <- valves, to <- tos]
-        non0Labels = [from | Valve from rate _ <- valves, rate > 0]
+        non0Labels = startLabel : [from | Valve from rate _ <- valves, rate > 0]
 
 -- travelCosts :: [Valve] -> TravelCost
 
--- test/Day16Spec.hs
+++ test/Day16Spec.hs
@@ -17,9 +17,19 @@ valveStrs = [
 
 valves = fmap readValve valveStrs
 
+
+valveShortestPaths :: [(Label, Label, Int)]
+valveShortestPaths =
+  fmap (\ (from, to, l) -> (mkLabel from, mkLabel to, l))
+  [("AA","BB",2),("AA","CC",3),("AA","DD",2),("AA","EE",3),("AA","HH",6),("AA","JJ",3),("BB","AA",2),("BB","CC",2),("BB","DD",3),("BB","EE",4),("BB","HH",7),("BB","JJ",4),("CC","AA",3),("CC","BB",2),("CC","DD",2),("CC","EE",3),("CC","HH",6),("CC","JJ",5),("DD","AA",2),("DD","BB",3),("DD","CC",2),("DD","EE",2),("DD","HH",5),("DD","JJ",4),("EE","AA",3),("EE","BB",4),("EE","CC",3),("EE","DD",2),("EE","HH",4),("EE","JJ",5),("HH","AA",6),("HH","BB",7),("HH","CC",6),("HH","DD",5),("HH","EE",4),("HH","JJ",8),("JJ","AA",3),("JJ","BB",4),("JJ","CC",5),("JJ","DD",4),("JJ","EE",5),("JJ","HH",8)]
+
 spec :: Spec
 spec = do
   describe "readValve" $ do
     it "should read valveStrs correctly" $ do
       head valves `shouldBe` mkValve "AA" 0 ["DD", "II", "BB"]
       last valves `shouldBe` mkValve "JJ" 21 ["II"]
+
+  describe "shortestPaths" $ do
+    it "should provide shortest paths for > 0 valves and start label" $ do
+      (fmap (\p -> (head p, last p, length p)) . shortestPaths $ valves) `shouldBe` valveShortestPaths
